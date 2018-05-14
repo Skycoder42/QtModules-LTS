50b639a681679480d41d79c67eaf42619bf43fd2
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-05-15 00:41:18 +0200

    ...

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -9,10 +9,9 @@ if "%PLATFORM%" == "static" set LTS_MODS=qtrestclient
 setlocal
 for %%m in (%LTS_MODS%) do (
 	cd %%m
-	xcopy /e /s /t /i /f ..\qtmodules-travis\* qtmodules-travis\ || exit /B 1
-	dir /s
+	xcopy /e /s /i /f ..\qtmodules-travis\* qtmodules-travis\ || exit /B 1
 	echo Building %%m ...
 	call .\qtmodules-travis\ci\win\build.bat || exit /B 1
-	xcopy /e /s /t /i /f install\* C:\ || exit /B 1
+	xcopy /e /s /i /f install\* C:\ || exit /B 1
 	cd ..
 )
