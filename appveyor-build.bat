cb1869386b8a75d102cf76b5fdcc889c830b7c67
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-05-15 00:19:53 +0200

    dir recursive for more info

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -8,12 +8,9 @@ if "%PLATFORM%" == "static" set LTS_MODS=qtrestclient
 
 setlocal
 for %%m in (%LTS_MODS%) do (
-	@echo on
-	dir
 	cd %%m
-	dir
 	xcopy /e /s /t /i /f ..\qtmodules-travis\* qtmodules-travis\ || exit /B 1
-	dir
+	dir /s
 	echo Building %%m ...
 	call .\qtmodules-travis\ci\win\build.bat || exit /B 1
 	xcopy /e /s /t /i /f install\* C:\ || exit /B 1
