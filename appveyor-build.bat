11e21e7d0b9a5f67d8f045753a1fb0367ff5b36a
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-05-15 00:07:09 +0200

    use gfind on mac, more verbose av

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -8,8 +8,12 @@ if "%PLATFORM%" == "static" set LTS_MODS=qtrestclient
 
 setlocal
 for %%m in (%LTS_MODS%) do (
+	@echo on
+	dir
 	cd %%m
+	dir
 	xcopy /e /s /t /i /f ..\qtmodules-travis\* qtmodules-travis\ || exit /B 1
+	dir
 	echo Building %%m ...
 	call .\qtmodules-travis\ci\win\build.bat || exit /B 1
 	xcopy /e /s /t /i /f install\* C:\ || exit /B 1
--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 set -e
 
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+	brew upgrade findutils || brew install findutils
+	FIND=gfind
+	XARGS=gxargs
+else
+	FIND=find
+	XARGS=xargs
+fi
+
 for mod in $LTS_MODS; do
 	mkdir -p install/$mod
 	pushd $mod
 	echo Packaging $mod...
 	./qtmodules-travis/ci/$TRAVIS_OS_NAME/upload-prepare.sh
 	#rename build_ ${mod}_build_ install/*.tar.xz
-	find install/ -type f -name "*.tar.xz" -maxdepth 1 -printf "%f\0" | xargs --null -I{} mv install/{} "install/${mod}_{}"
+	$FIND install/ -type f -name "*.tar.xz" -maxdepth 1 -printf "%f\0" | $XARGS --null -I{} mv install/{} "install/${mod}_{}"
 	cp install/*.tar.xz ../install
 	popd
 done
 
-find install/
+$FIND install/
