e558770423470adde66d9a53156ad34c8386f16a
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-05-14 16:44:51 +0200

    try with module copying

--- travis-build.sh
+++ travis-build.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 set -e
 
-for mod in $LTS_MODS; do
-	pushd $mod
-	cp -R ../qtmodules-travis qtmodules-travis
-	echo Building $mod...
-	./qtmodules-travis/ci/$TRAVIS_OS_NAME/build.sh
-	sudo cp -R install/* /
-	popd
-done
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+	# inject code to keep qt mods over each image
+	buildFile=qtmodules-travis/ci/$TRAVIS_OS_NAME/build-docker.sh
+	mv $buildFile /tmp/build-docker.sh
+	echo '#!/bin/bash' > $buildFile
+	echo 'set -e' >> $buildFile
+	echo 'cp -R ./qtmods/* /' >> $buildFile
+	cat /tmp/build-docker.sh >> $buildFile
+
+	for mod in $LTS_MODS; do
+		pushd $mod
+		cp -R ../qtmodules-travis qtmodules-travis
+		cp -R ../qtmods qtmods
+		echo Building $mod...
+		./qtmodules-travis/ci/$TRAVIS_OS_NAME/build.sh
+		cp -R install/* /qtmods/
+		popd
+	done
+else
+	for mod in $LTS_MODS; do
+		pushd $mod
+		ln -s ../qtmodules-travis qtmodules-travis
+		echo Building $mod...
+		./qtmodules-travis/ci/$TRAVIS_OS_NAME/build.sh
+		sudo cp -R install/* /
+		popd
+	done
+fi
