0785ee81857c037309928272cf6cad8cc98a4c01
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-05-14 20:55:38 +0200

    try with fixed autoupdater

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-mkdir -p install
 for mod in $LTS_MODS; do
+	mkdir -p install/$mod
 	pushd $mod
 	echo Packaging $mod...
 	./qtmodules-travis/ci/$TRAVIS_OS_NAME/upload-prepare.sh
-	cp install/*.tar.xz ../install/
+	cp install/*.tar.xz ../install/$mod
 	popd
 done
 
-ls -lsa install/
+find install/
