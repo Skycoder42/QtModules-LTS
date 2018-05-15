9325c8cf26a6f78ecd169cce93eaf92503c1ec44
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-05-15 11:16:52 +0200

    appveyor fix qt mods and preparing
    also remove debug logging

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -1,10 +1,12 @@
-@echo on
+@echo off
 
 if "%PLATFORM%" == "winrt_x64_msvc2017" set LTS_MODS=qtjsonserializer qtrestclient qtdatasync
 if "%PLATFORM%" == "winrt_x86_msvc2017" set LTS_MODS=qtjsonserializer qtrestclient qtdatasync
 if "%PLATFORM%" == "winrt_armv7_msvc2017" set LTS_MODS=qtjsonserializer qtrestclient
 if "%PLATFORM%" == "mingw53_32" set LTS_MODS=qtjsonserializer qtrestclient qtautoupdater
 if "%PLATFORM%" == "static" set LTS_MODS=qtrestclient
+
+call .\qtdatasync\src\3rdparty\cryptopp\appveyor.bat || exit /B 1
 
 setlocal
 for %%m in (%LTS_MODS%) do (
@@ -13,12 +15,10 @@ for %%m in (%LTS_MODS%) do (
 	
 	cd %%m
 	:: copy over ci + build
-	xcopy /e /s /i /f ..\qtmodules-travis\* qtmodules-travis\ || exit /B 1
+	xcopy /e /s /i ..\qtmodules-travis\* qtmodules-travis\ || exit /B 1
 	echo Building %%m ...
 	call .\qtmodules-travis\ci\win\build.bat || exit /B 1
 	:: copy install stuff into qt install
-	dir /s install
-	xcopy /e /s /i /f install\* C:\ || exit /B 1
-	echo cpdone
+	xcopy /e /s /i install\* C:\ || exit /B 1
 	cd ..
 )
