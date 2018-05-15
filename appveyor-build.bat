6636bad894712722d97eb073d8aed9e5f6b1481d
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-05-15 11:00:09 +0200

    fixup av build

--- appveyor-build.bat
+++ appveyor-build.bat
@@ -8,13 +8,16 @@ if "%PLATFORM%" == "static" set LTS_MODS=qtrestclient
 
 setlocal
 for %%m in (%LTS_MODS%) do (
+	:: prepare install dir link
+	mklink /D C:\projects\%%m C:\projects\qtmodules-lts\%%m
+	
 	cd %%m
+	:: copy over ci + build
 	xcopy /e /s /i /f ..\qtmodules-travis\* qtmodules-travis\ || exit /B 1
 	echo Building %%m ...
 	call .\qtmodules-travis\ci\win\build.bat || exit /B 1
-	dir ..
-	move ..\install install || exit /B 1
-	dir install
+	:: copy install stuff into qt install
+	dir /s install
 	xcopy /e /s /i /f install\* C:\ || exit /B 1
 	echo cpdone
 	cd ..
